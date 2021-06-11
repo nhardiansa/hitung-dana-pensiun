8ec7ced1222ce94b68875aed550e5f6287f2b19e
nhardiansa <nhardiansa@gmail.com>
2021-06-11 22:26:58 +0800

    sedang membuat bagian navbar untuk mobile

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ const pengeluaranTahun = document.querySelector("#spending")
 const pengeluaranBulan = document.querySelector(".spending-now .input input:nth-child(2)")
 
 function getValue(e){
-    let num = e.target.value;
+    let num = e.target.value
+                .toLowerCase()
+                .split(".")
+                .filter((word) => !isNaN(word))
+                .join("")
     
-    pengeluaranTahun.value = "Rp."+num
+
+    // num = num.split(".")
+    // num = num.
+    
+    console.log(parseInt(num))
+    // console.log(num)
 }
 
-// pengeluaranTahun.addEventListener("change", getValue)
+pengeluaranTahun.addEventListener("change", getValue)
